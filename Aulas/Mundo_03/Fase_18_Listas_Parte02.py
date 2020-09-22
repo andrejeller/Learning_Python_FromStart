8e5e01a6a8a7086f3c2049cb88147d65cc09de51
andrejeller <jeller.andre@gmail.com>
2020-09-22 18:55:58 -0300

    Arrumando nomes

---
+++ Aulas/Mundo_03/Fase_18_Listas_Parte02.py
@@ -0,0 +1,49 @@
+"""
+O que foi visto na aula 17.
+- Listas parte 02
+
+"""
+
+teste = list()
+teste.append('André')
+teste.append(22)
+print(teste)
+
+galera = list()
+galera.append(teste[:])  # galera.append(teste) - Precisa ser feito uma copia
+teste[0] = 'Maria'
+teste[1] = 22
+galera.append(teste[:])  # galera.append(teste) - Precisa ser feito uma copia
+print(galera)
+
+# --
+print('-'*50)
+galera = [['João', 19], ['Ana', 33], ['Joaquim', 13], ['Maria', 45]]
+print(galera[0])
+print(galera[0][0])
+print(galera[2][1])
+
+for p in galera:
+    # print(p)
+    # print(p[0])
+    print(f'{p[0]} tem {p[1]} anos de idade.')
+
+# --
+print('-'*50)
+galera = list()
+dado = list()
+
+for i in range(0, 3):
+    dado.append(str(input('Nome: ')))
+    dado.append(int(input('Idade: ')))
+    galera.append(dado[:])  # passando uma cópia
+    dado.clear()
+print(galera)
+for p in galera:
+    if p[1] >= 21:
+        print(f'{p[0]} é maior de idade.')
+    else:
+        print(f'{p[0]} é menor de idade.')
+
+# --
+print('-'*50)
